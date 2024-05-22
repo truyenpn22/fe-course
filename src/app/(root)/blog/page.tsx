18170b1f5b3cf4ce6f2160f38933e443f23361e4
truyenpn22 <phantruyen19722@gmail.com>
2024-05-22 16:44:03 +0700

    upcode

---
+++ src/app/(root)/blog/page.tsx
@@ -0,0 +1,12 @@
+import Card from "@/components/ui/Card"
+
+const blog = () => {
+    return (
+        <div>
+            <Card />
+
+        </div>
+    )
+}
+
+export default blog
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,6 +13,9 @@ const config: Config = {
         'gradient-conic':
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
+      minHeight: {
+        'screen-minus-72': 'calc(100vh - 72px)',
+      },
     },
   },
   plugins: [],
